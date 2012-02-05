73f688d267d52052cec0ee0a706f9a8a0fa0f316
seavenois@gmail.com <seavenois@gmail.com@d8e73f47-fd1b-119d-532e-8769c2cfb0bb>
2012-02-05 00:11:11 +0000

    Fixes. App is beta now

--- src/com/seavenois/tetris/HighScores.java
+++ src/com/seavenois/tetris/HighScores.java
@@ -11,7 +11,7 @@ public class HighScores extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
 		
 		//TextViews
-		TextView hs1, hs2, hs3;
+		TextView hs1, hs2, hs3, hs1d, hs2d, hs3d;
 		
 		//Saved values
 		SharedPreferences highScores;
@@ -25,15 +25,23 @@ public class HighScores extends Activity{
         hs1 = (TextView) findViewById(R.id.textViewHighScore1);
         hs2 = (TextView) findViewById(R.id.textViewHighScore2);
         hs3 = (TextView) findViewById(R.id.textViewHighScore3);
+        hs1d = (TextView) findViewById(R.id.textViewHighScore1Date);
+        hs2d = (TextView) findViewById(R.id.textViewHighScore2Date);
+        hs3d = (TextView) findViewById(R.id.textViewHighScore3Date);
         
         //Load high scores
         highScores = getSharedPreferences("highScores", 0);
-		if (highScores.getInt("hScore1", 0) > 0)
+		if (highScores.getInt("hScore1", 0) > 0){
 			hs1.setText(getResources().getString(R.string.first) + ": " + Integer.toString(highScores.getInt("hScore1", 0)));
-		if (highScores.getInt("hScore2", 0) > 0)
-			hs2.setText(getResources().getString(R.string.first) + ": " + Integer.toString(highScores.getInt("hScore2", 0)));
-		if (highScores.getInt("hScore3", 0) > 0)
-			hs3.setText(getResources().getString(R.string.first) + ": " + Integer.toString(highScores.getInt("hScore3", 0)));
-		
+			hs1d.setText(highScores.getString("hScore1Date", " "));
+		}
+		if (highScores.getInt("hScore2", 0) > 0){
+			hs2.setText(getResources().getString(R.string.second) + ": " + Integer.toString(highScores.getInt("hScore2", 0)));
+			hs2d.setText(highScores.getString("hScore2Date", " "));
+		}
+		if (highScores.getInt("hScore3", 0) > 0){
+			hs3.setText(getResources().getString(R.string.third) + ": " + Integer.toString(highScores.getInt("hScore3", 0)));
+			hs3d.setText(highScores.getString("hScore3Date", " "));
+		}		
 	}
 }
